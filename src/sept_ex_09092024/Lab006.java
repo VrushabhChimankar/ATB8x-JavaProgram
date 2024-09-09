df33652049f826febac1a92f86fc7bea4cbdbf5d
SeelaCharu <seelacharu151@gmiail.com>
2024-09-09 21:24:42 +0530

    Add all files.

---
+++ src/sept_ex_09092024/Lab006.java
@@ -0,0 +1,14 @@
+package sept_ex_09092024;
+
+public class Lab006 {
+
+    public static void main(String[] args) {
+
+        //Variables is used to store values
+        //int a = 10; where a is variable & stores 10 value & int is a datatype
+        boolean is_vrushabh_single = true;
+        //public, static, void, boolean, true, false these are the keywords always starts with lowercase character.
+
+        System.out.println(is_vrushabh_single);
+    }
+}
