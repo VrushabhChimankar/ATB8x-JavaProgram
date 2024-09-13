8be25494a7ade26927e5260b5979e7d82f2953f8
SeelaCharu <seelacharu151@gmiail.com>
2024-09-13 22:29:55 +0530

    All the files

---
+++ src/sept_ex_13092024/Lab012.java
@@ -0,0 +1,22 @@
+package sept_ex_13092024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+
+        //Formatting for String
+        String name = "Vrushabh";
+//        System.out.printf("My name is %s", name);
+        String lastname = "Chimankar";
+        System.out.printf("My name is %s & lastname is %s",name,lastname);
+
+        System.out.println();
+        //Formatting for float
+        float f = 10.2f;
+        System.out.printf("Value of float is %f",f);
+
+        //Formatting for double
+        System.out.println();
+        double f2 = 10.1224231;
+        System.out.printf("Value of double is %f", f2);
+    }
+}
