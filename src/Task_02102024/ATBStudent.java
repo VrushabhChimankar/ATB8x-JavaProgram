6ed06366f33a33125b57c6b03c2fdfb3c0788abe
SeelaCharu <seelacharu151@gmiail.com>
2024-10-03 19:26:24 +0530

    All the files - 3 Oct

---
+++ src/Task_02102024/ATBStudent.java
@@ -0,0 +1,15 @@
+package Task_02102024;
+
+public class ATBStudent {
+
+
+        //Here are the 5 objects of the ATBStudent Class
+        String name;
+        int age;
+        String lastname;
+        long mobile_num;
+        String gender;
+
+
+
+}
---
+++ src/Task_02102024/CallDetails.java
@@ -0,0 +1,23 @@
+package Task_02102024;
+
+public class CallDetails {
+
+    public static void main(String[] args) {
+
+
+        ATBStudent obj1 = new ATBStudent();
+        obj1.name = "Vrushabh";
+
+        ATBStudent obj2 = new ATBStudent();
+        obj2.gender = "Male";
+
+        ATBStudent Obj3 = new ATBStudent();
+        Obj3.age = 24;
+
+        ATBStudent obj4 = new ATBStudent();
+        obj4.lastname = "Chimankar";
+
+        ATBStudent Obj5 = new ATBStudent();
+        Obj5.mobile_num = 9876543210l;
+    }
+}
